41e80504f34525927905dfb1bbefb461d3cbb3d6
MozYiF <95163973+MOZyIF@users.noreply.github.com>
2023-05-11 01:19:12 +0800

    Delete src/main directory

--- src/main/java/studyrooms/dao/BlackLogsDao.java
+++
@@ -1,9 +0,0 @@
-package studyrooms.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.springframework.stereotype.Repository;
-import studyrooms.entity.BlackLogs;
-
-@Repository("blackLogsDao")
-public interface BlackLogsDao extends BaseMapper<BlackLogs> {
-}
