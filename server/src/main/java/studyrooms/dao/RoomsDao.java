ba99e2c1bbd0044531b3e7b955f8cd9c7f2c5d39
MozYiF <95163973+MOZyIF@users.noreply.github.com>
2023-05-10 22:48:04 +0800

    Delete server directory

--- server/src/main/java/studyrooms/dao/RoomsDao.java
+++
@@ -1,9 +0,0 @@
-package studyrooms.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.springframework.stereotype.Repository;
-import studyrooms.entity.Rooms;
-
-@Repository("roomsDao")
-public interface RoomsDao extends BaseMapper<Rooms> {
-}
